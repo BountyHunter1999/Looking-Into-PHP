7023f8dbf1d5fc58f593731802ba3a5a18ded52f
shrish <adhikari.shrish1@gmail.com>
2024-07-29 19:01:16 +0545

    Added Functions

--- demo/index.php
+++ demo/index.php
@@ -16,17 +16,48 @@
   <body>
     <h1>Recommended Mangas</h1>
     <?php
-    $mangas = [
-      "One Piece",
-      "My Hero Academia",
-      "Hunter_x_Hunter",
-      "JoJo"
-    ];
+      $mangas = [
+          [
+              "name" => "One Piece",
+              "author" => "Eiichiro Oda",
+              "releaseYear" => 1997,
+              "link" => "https://www.viz.com/one-piece"
+          ],
+          [
+              "name" => "Hunter x Hunter",
+              "author" => "Yoshihiro Togashi",
+              "releaseYear" => 1998,
+              "link" => "https://www.viz.com/hunter-x-hunter"
+          ],
+          [
+              "name" => "Dragon Ball Z",
+              "author" => "Akira Toriyama",
+              "releaseYear" => 1989,
+              "link" => "https://www.viz.com/dragon-ball"
+          ],
+          [
+            "name" => "Dr. Slump",
+            "author" => "Akira Toriyama",
+            "releaseYear" => 1980,
+            "link" => "https://www.viz.com/dr-slump"
+          ]
+      ];
 
+      function filterByAuthor($mangas, $author) {
+        $filteredMangas = [];
+        foreach ($mangas as $manga) {
+          if ($manga['author'] === $author) {
+            $filteredMangas[] = $manga;
+          }
+        }
+          return $filteredMangas;
+      }
     ?>
     <ul>
-      <?php foreach ($mangas as $manga) : ?>
-        <li><?= $manga; ?></li>
+      <?php foreach (filterByAuthor($mangas, "Akira Toriyama") as $manga) : ?>
+        <?php if ($manga['author'] === "Akira Toriyama") : ?>
+          <li><?= "{$manga['author']} wrote {$manga['name']} at {$manga['releaseYear']}"; ?></li>
+        <?php endif; ?>
       <?php endforeach; ?>
     </ul>
   </body>
