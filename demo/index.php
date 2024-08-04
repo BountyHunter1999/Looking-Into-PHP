5fcac8c221affcf3de0202395d56a4df40ec93fb
shrish <adhikari.shrish1@gmail.com>
2024-08-04 10:45:22 +0545

    First fetch

--- demo/index.php
+++ demo/index.php
@@ -21,6 +21,8 @@ $statement = $pdo->prepare("SELECT * FROM posts;");
 
 $statement->execute();
 
-$posts = $statement -> fetchAll();
+$posts = $statement -> fetchAll(PDO::FETCH_ASSOC);
 
-dd($posts);
+foreach ($posts as $post) {
+    echo "<li>". $post["title"] ."</li>";
+}
