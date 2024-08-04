e31c6613f59a7b3f1c31877c2fe0d421a422f20f
shrish <adhikari.shrish1@gmail.com>
2024-08-04 07:17:20 +0545

    Used custom logic to update current page indication

--- demo/index.php
+++ demo/index.php
@@ -1,44 +1,7 @@
 <?php
 
-$mangas = [
-    [
-        "name" => "One Piece",
-        "author" => "Eiichiro Oda",
-        "releaseYear" => 1997,
-        "link" => "https://www.viz.com/one-piece"
-    ],
-    [
-        "name" => "Hunter x Hunter",
-        "author" => "Yoshihiro Togashi",
-        "releaseYear" => 1998,
-        "link" => "https://www.viz.com/hunter-x-hunter"
-    ],
-    [
-        "name" => "Dragon Ball Z",
-        "author" => "Akira Toriyama",
-        "releaseYear" => 1989,
-        "link" => "https://www.viz.com/dragon-ball"
-    ],
-    [
-    "name" => "Dr. Slump",
-    "author" => "Akira Toriyama",
-    "releaseYear" => 1980,
-    "link" => "https://www.viz.com/dr-slump"
-    ]
-];
+require "functions.php";
 
-$filter = function ($items, $fn) {
-    $filteredItems = [];
-    foreach ($items as $item) {
-        if ($fn($item)) {
-        $filteredItems[] = $item;
-        }
-    }
-    return $filteredItems;
-};
+$heading = "Home";
 
-$filteredItems = $filter($mangas, function ($manga) {
-    return $manga['releaseYear'] >= 1980 and $manga['releaseYear'] <= 1997;
-});
-
-require "index.view.php";
+require "views/index.view.php";
