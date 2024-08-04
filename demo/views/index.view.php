b34a6248eaf9caa27659671c3eaf889422125a11
shrish <adhikari.shrish1@gmail.com>
2024-08-04 07:57:15 +0545

    Used routers and controllers

--- demo/views/index.view.php
+++ demo/views/index.view.php
@@ -6,7 +6,7 @@
 <main>
   <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
     <!-- Your content -->
-    <p>Hello, This is my home page</p>
+    <h1 class="text-2xl font-bold">Hello, This is my home page</h1>
   </div>
 </main>
 <?php require("partials/footer.php") ?>
--- demo/views/partials/nav.php
+++ demo/views/partials/nav.php
@@ -19,13 +19,13 @@
                     >Home</a
                   >
                   <a
-                    href="/about.php"
-                    class="<?= urlIs('about.php') ? 'bg-gray-900 text-white' : 'text-gray-300';?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                    href="/about"
+                    class="<?= urlIs('about') ? 'bg-gray-900 text-white' : 'text-gray-300';?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                     >About</a
                   >
                   <a
-                    href="/contact.php"
-                    class="<?= urlIs('contact.php') ? 'bg-gray-900 text-white' : 'text-gray-300';?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                    href="/contact"
+                    class="<?= urlIs('contact') ? 'bg-gray-900 text-white' : 'text-gray-300';?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                     >Contact</a
                   >
                 </div>
