88e1bc6cd6b0a2b78b6acb84088830a5ec948037
shrish <adhikari.shrish1@gmail.com>
2024-08-09 17:14:23 +0545

    Added some style to paragraph

--- note-app/views/note.view.php
+++ note-app/views/note.view.php
@@ -6,13 +6,12 @@
 <main>
   <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
     <!-- Your content -->
+    <p class="mb-6">
+      <a href="/notes" class="text-2xl font-bold underline">Go Back</a>
+    </p>
+
     <h1 class="text-2xl font-bold">Hello, This is my note #<?= $note['id'] ?></h1>
-      <li>
-        <a href="/note?id=<?= $note['id'] ?>" class="text-blue-500 hover:underline">
-          <?= $note['body'] ?>
-          <?= $note['id'] ?>
-        </a>
-      </li>
+    <p class="text-lg mt-6 italic"><?= $note['body'] ?></p>
   </div>
 </main>
 <?php require("partials/footer.php") ?>
