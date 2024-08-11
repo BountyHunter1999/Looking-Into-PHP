6345eec996e42eabfc04678088fa8b5500187fb2
shrish <adhikari.shrish1@gmail.com>
2024-08-11 18:33:02 +0545

    One Request, One Controller implemented

--- note-app/controllers/notes/destroy.php
+++ note-app/controllers/notes/destroy.php
@@ -6,24 +6,16 @@ $config = require base_path("config.php");
 
 $currentUserId = 1;
 
-
-
-// get variables passed to the endpointr
-// dd($_GET['id']);
-
 $DB_USER = $_ENV["DB_USER"] ?? "hariom";
 $DB_PASSWORD = $_ENV["DB_PASSWORD"] ?? "om123!";
 $db = New Database($config["database"], username: $DB_USER, password: $DB_PASSWORD);
 
 
-$note = $db->query("SELECT * FROM notes WHERE id = :id", ["id" =>  $_GET['id']])->findOrFail();
+$note = $db->query("SELECT * FROM notes WHERE id = :id", ["id" =>  $_POST['id']])->findOrFail();
 
 authorize($note['user_id'] === $currentUserId);
 
-require view("notes/show.view.php", [
-    "heading" => "Note",
-    "note" => $note
-]);
+$db->query("DELETE FROM notes WHERE id = :id", ["id" => $_POST["id"]]);
 
-header("location: /notes");
-exit();
+header('location: /notes');
+exit(); // ensure redirect happens cleanly and no unintended code execution occurs afterward.
