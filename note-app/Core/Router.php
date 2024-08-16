f591446797361d0f45e42e33b5546d25ec0c8dc1
shrish <adhikari.shrish1@gmail.com>
2024-08-16 09:04:06 +0545

    Added simple middleware

--- note-app/Core/Router.php
+++ note-app/Core/Router.php
@@ -11,32 +11,48 @@ class Router {
             "controller" => $controller,
             "method" => $method
         ];
+
+        return $this;
     }
     // public function add($uri, $controller, $method){
     //     $this->routes[] = compact("uri", "controller", "method");
     // }
     public function get($uri, $controller) {
-        $this->add($uri,$controller, "GET");
+        return $this->add($uri,$controller, "GET");
     }
 
     public function post($uri, $controller) {
-        $this->add($uri, $controller, "POST");
+        return $this->add($uri, $controller, "POST");
     }
     public function delete($uri, $controller) {
-        $this->add($uri,$controller,"DELETE");
+        return $this->add($uri,$controller,"DELETE");
     }
 
     public function patch($uri, $controller) {
-        $this->add($uri, $controller,"PATCH");
+        return $this->add($uri, $controller,"PATCH");
     }
     public function put($uri, $controller) {
-        $this->add($uri,$controller,"PUT");
+        return $this->add($uri,$controller,"PUT");
+    }
+
+    public function only($key) {
+        $this->routes[array_key_last($this->routes)]['middleware'] = $key;
+        return $this;
     }
 
     public function route($uri, $method = "GET") {
         // dd($this->routes);
         foreach ($this->routes as $route) {
             if ($route["uri"] === $uri && $route['method'] === strtoupper($method)) {
+                // apply the middleware
+                if (key_exists("middleware", $route)) {
+                    if ($route["middleware"] ?? $route['middleware'] === 'guest') {
+                        if ($_SESSION['user'] ?? false) {
+                            header('location: /');
+                            exit();
+                        }
+                    }
+                }
                 return require base_path($route['controller']);
             }
         }
