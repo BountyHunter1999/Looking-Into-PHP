1c466cef0f64826d5cb55d65f824c16614113abd
shrish <adhikari.shrish1@gmail.com>
2024-08-17 12:57:46 +0545

    Fixed styling issue

--- note-app/views/index.view.php
+++ note-app/views/index.view.php
@@ -5,7 +5,7 @@
 
 <main>
   <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-    <!-- <h1 class="text-2xl font-bold">Hello, This is my home page</h1> -->
+    <h1 class="text-2xl font-bold">Hello <?= $_SESSION['user']['email'] ?? 'guest'   ?>, This is my home page</h1>
   </div>
 </main>
 <?php require base_path("views/partials/footer.php") ?>
