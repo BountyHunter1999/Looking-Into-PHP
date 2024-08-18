0d792f86d269a12e04f72026ada9b1b8c36d6acf
shrish <adhikari.shrish1@gmail.com>
2024-08-18 17:12:48 +0545

    Added logic so that notes is only visible to logged in user

--- note-app/views/partials/nav.php
+++ note-app/views/partials/nav.php
@@ -23,11 +23,13 @@
                     class="<?= urlIs('about') ? 'bg-gray-900 text-white' : 'text-gray-300';?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                     >About</a
                   >
-                  <a
-                    href="/notes"
-                    class="<?= urlIs('notes') ? 'bg-gray-900 text-white' : 'text-gray-300';?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                    >Notes</a
-                  >
+                  <?php if ($_SESSION['user'] ?? false) : ?>
+                    <a
+                      href="/notes"
+                      class="<?= urlIs('notes') ? 'bg-gray-900 text-white' : 'text-gray-300';?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                      >Notes</a
+                    >
+                  <?php endif; ?>
                   <a
                     href="/contact"
                     class="<?= urlIs('contact') ? 'bg-gray-900 text-white' : 'text-gray-300';?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
